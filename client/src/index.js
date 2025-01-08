baa40eff3be49d1ebe42a7b5c1efe2f8fe164031
cem-sucu <cems.cems.17@gmail.com>
2025-01-08 13:20:23 +0100

    reactDOM refactorisser suite a mise a jour de React

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Assure-toi d'importer depuis 'react-dom/client'
 
 import App from './App';
 
-ReactDOM.render(
+// Crée un root à partir de l'élément DOM
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Utilise root.render() pour rendre l'application
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
